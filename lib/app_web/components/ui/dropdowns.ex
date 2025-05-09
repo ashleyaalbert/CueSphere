970c66c23724842928a196c8a18292e5117df2db
ashley <aaa016@bucknell.edu>
2025-05-08 22:43:14 -0400

    Dropdown Added

--- lib/app_web/components/ui/dropdowns.ex
+++ lib/app_web/components/ui/dropdowns.ex
@@ -3,13 +3,21 @@ defmodule AppWeb.Components.UI.Dropdowns do
 
   attr :id, :string, required: true
   attr :label, :string, default: "Dropdown"
-  attr :items, :list, default: [
-    %{label: "Dashboard", href: "#"},
-    %{label: "Settings", href: "#"},
-    %{label: "Earnings", href: "#"},
-    %{label: "Sign out", href: "#"}
-  ]
+  attr :class, :string, default: ""
 
+  attr :button_class, :string,
+    default:
+      "text-gray-300 bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:text-gray-900 dark:bg-gray-300 dark:hover:bg-gray-400 dark:focus:ring-gray-200"
+
+  attr :items, :list,
+    default: [
+      %{label: "Dashboard", href: "#"},
+      %{label: "Settings", href: "#"},
+      %{label: "Earnings", href: "#"},
+      %{label: "Sign out", href: "#"}
+    ]
+
+  # how to use:
   # import AppWeb.Components.UI.Dropdowns
   # <.dropdown id="myDropdown" label="Menu" items={[
   #   %{label: "Profile", href: "/about"},
@@ -19,16 +27,23 @@ defmodule AppWeb.Components.UI.Dropdowns do
 
   def dropdown(assigns) do
     ~H"""
-    <div class="relative inline-block text-left">
-      <button
-        id={@id <> "Button"}
-        data-dropdown-toggle={@id}
-        type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        <%= @label %>
-        <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+    <div class="relative inline-block text-left #{@class}">
+      <button id={@id <> "Button"} data-dropdown-toggle={@id} type="button" class={@button_class}>
+        {@label}
+        <svg
+          class="w-2.5 h-2.5 ms-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 4 4 4-4"
+          />
         </svg>
       </button>
 
@@ -36,12 +51,36 @@ defmodule AppWeb.Components.UI.Dropdowns do
         id={@id}
         class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700"
       >
-        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby={@id <> "Button"}>
+        <ul
+          class="py-2 text-sm text-gray-300 dark:text-gray-900 bg-gray-800 dark:bg-gray-300"
+          aria-labelledby={@id <> "Button"}
+        >
           <%= for item <- @items do %>
             <li>
-              <a href={item.href} class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                <%= item.label %>
-              </a>
+              <%= cond do %>
+                <% item[:method] -> %>
+                  <.link
+                    href={item[:href]}
+                    method={item.method}
+                    class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400"
+                  >
+                    {item.label}
+                  </.link>
+                <% item[:phx_click] -> %>
+                  <button
+                    phx-click={item.phx_click}
+                    class="block w-full text-left px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400"
+                  >
+                    {item.label}
+                  </button>
+                <% true -> %>
+                  <.link
+                    href={item[:href]}
+                    class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400"
+                  >
+                    {item.label}
+                  </.link>
+              <% end %>
             </li>
           <% end %>
         </ul>
--- lib/app_web/components/ui/navbar.ex
+++ lib/app_web/components/ui/navbar.ex
@@ -8,7 +8,9 @@ defmodule AppWeb.Components.UI.Navbar do
     statics: AppWeb.static_paths()
 
   alias Phoenix.LiveView.JS
+  import Phoenix.LiveView
   alias AppWeb.Components.UI.Modal
+  import AppWeb.Components.UI.Dropdowns
 
   @doc """
   Renders a navbar.
@@ -21,107 +23,120 @@ defmodule AppWeb.Components.UI.Navbar do
 
   def navbar(assigns) do
     ~H"""
-  <nav class="bg-gray-800 dark:bg-gray-300">
-    <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+    <nav class="bg-gray-800 dark:bg-gray-300">
+      <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
 
-      <!-- Mobile Menu Button -->
-      <button
-        id="menu-button"
-        type="button"
-        class="md:hidden p-2 w-10 h-10 text-gray-500 rounded-lg hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"
-        aria-controls="menu"
-        aria-expanded="false"
-        phx-click={toggle_menu()}
-        tabindex="1"
-      >
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-        </svg>
-      </button>
+        <!-- Mobile Menu Button -->
+        <button
+          id="menu-button"
+          type="button"
+          class="md:hidden p-2 w-10 h-10 text-gray-500 rounded-lg hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"
+          aria-controls="menu"
+          aria-expanded="false"
+          phx-click={toggle_menu()}
+          tabindex="1"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
 
-      <div id="menu" class="hidden w-full md:flex md:items-center">
-        <ol class="flex flex-col md:flex-row md:space-x-8 text-gray-300 dark:text-gray-900">
+        <div id="menu" class="hidden w-full md:flex md:items-center">
+          <ol class="flex flex-col md:flex-row md:space-x-8 text-gray-300 dark:text-gray-900">
+            <.dropdown
+              id="generalDropdown"
+              label="General"
+              items={[
+                %{label: "Home", href: ~p"/"},
+                %{label: "About", href: ~p"/about"},
+                %{label: "Facts", href: ~p"/facts"},
+                %{label: "Fargo Rate", href: ~p"/fargo-rate"},
+                %{label: "Purchase", href: ~p"/purchase"}
+              ]}
+            />
 
-          <li class="relative group">
-            <.link href={~p"/"} tabindex="1" class="px-4 py-2 block hover:bg-gray-700 dark:hover:bg-gray-400 rounded-md transition-all">{gettext("General")}</.link>
-            <ul class="hidden group-hover:block group-hover:delay-150 md:absolute md:left-0 md:bg-gray-800 md:dark:bg-gray-300 md:mt-1 md:w-44 md:rounded-md md:shadow-lg md:border md:border-gray-700 md:dark:border-gray-400
-              md:p-0 p-2 bg-gray-900 dark:bg-gray-200 rounded-md md:shadow-none">
-              <li><.link href={~p"/about"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("About")}</.link></li>
-              <li><.link href={~p"/facts"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Facts")}</.link></li>
-              <li><.link href={~p"/fargo-rate"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Fargo Rate")}</.link></li>
-              <li><.link href={~p"/purchase"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Purchase")}</.link></li>
-            </ul>
-          </li>
+            <.dropdown
+              id="learnDropdown"
+              label="Learn"
+              items={[
+                %{label: "Tutorials", href: ~p"/tutorials"},
+                %{label: "Drills", href: ~p"/drills"},
+                %{label: "Books", href: ~p"/books"},
+                %{label: "Movies", href: ~p"/movies"}
+              ]}
+            />
 
-          <li class="relative group">
-            <.link tabindex="1" class="px-4 py-2 block hover:bg-gray-700 dark:hover:bg-gray-400 rounded-md transition-all">{gettext("Learn")}</.link>
-            <ul class="hidden group-hover:block group-hover:delay-150 md:absolute md:left-0 md:bg-gray-800 md:dark:bg-gray-300 md:mt-1 md:w-44 md:rounded-md md:shadow-lg md:border md:border-gray-700 md:dark:border-gray-400
-              md:p-0 p-2 bg-gray-900 dark:bg-gray-200 rounded-md md:shadow-none">
-              <li><.link href={~p"/tutorials"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Tutorials")}</.link></li>
-              <li><.link href={~p"/drills"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Drills")}</.link></li>
-              <li><.link href={~p"/books"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Books")}</.link></li>
-              <li><.link href={~p"/movies"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Movies")}</.link></li>
-            </ul>
-          </li>
+            <.dropdown
+              id="playDropdown"
+              label="Play"
+              items={[
+                %{label: "Rules", href: ~p"/rules"},
+                %{label: "Games", href: ~p"/games"},
+                %{label: "Leagues", href: ~p"/leagues"},
+                %{label: "Tournaments", href: ~p"/tournaments"}
+              ]}
+            />
 
-          <li class="relative group">
-            <.link tabindex="1" class="px-4 py-2 block hover:bg-gray-700 dark:hover:bg-gray-400 rounded-md transition-all">{gettext("Play")}</.link>
-            <ul class="hidden group-hover:block group-hover:delay-150 md:absolute md:left-0 md:bg-gray-800 md:dark:bg-gray-300 md:mt-1 md:w-44 md:rounded-md md:shadow-lg md:border md:border-gray-700 md:dark:border-gray-400
-              md:p-0 p-2 bg-gray-900 dark:bg-gray-200 rounded-md md:shadow-none">
-              <li><.link href={~p"/rules"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Rules")}</.link></li>
-              <li><.link href={~p"/games"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Games")}</.link></li>
-              <li><.link href={~p"/leagues"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Leagues")}</.link></li>
-              <li><.link href={~p"/tournaments"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Tournaments")}</.link></li>
-            </ul>
-          </li>
+            <.dropdown
+              id="contactDropdown"
+              label="Contact Us"
+              items={[
+                %{label: "Messages", href: ~p"/messages"},
+                %{label: "Contact Form", phx_click: Modal.show_modal("contact-modal")}
+              ]}
+            />
 
-          <li class="relative group">
-            <button
-              type="button"
-              phx-click={Modal.show_modal("contact-modal")}
-              class="px-4 py-2 block hover:bg-gray-700 dark:hover:bg-gray-400 rounded-md transition-all"
-              tabindex="1"
-            >
-              {gettext("Contact Us")}
-            </button>
-            <ul class="hidden group-hover:block group-hover:delay-150 md:absolute md:left-0 md:bg-gray-800 md:dark:bg-gray-300 md:mt-1 md:w-44 md:rounded-md md:shadow-lg md:border md:border-gray-700 md:dark:border-gray-400
-              md:p-0 p-2 bg-gray-900 dark:bg-gray-200 rounded-md md:shadow-none">
-              <li><.link href={~p"/messages"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Messages")}</.link></li>
-            </ul>
-          </li>
-
-          <%= if @user_id do %>
-            <li class="relative group">
-              <.link href="#" class="px-4 py-2 block hover:bg-gray-700 dark:hover:bg-gray-400 rounded-md transition-all">
-                <%= @user_id.email %>
-              </.link>
-              <ul class="hidden group-hover:block group-hover:delay-150 md:absolute md:left-0 md:bg-gray-800 md:dark:bg-gray-300 md:mt-1 md:w-44 md:rounded-md md:shadow-lg md:border md:border-gray-700 md:dark:border-gray-400
-                md:p-0 p-2 bg-gray-900 dark:bg-gray-200 rounded-md md:shadow-none">
-                <li><.link tabindex="1" href={~p"/users/settings"} class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Settings")}</.link></li>
-                <li><.link id="logout-button" tabindex="1" href={~p"/users/log_out"} method="DELETE" phx-hook="LogoutButton" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Log out")}</.link></li>
-              </ul>
-            </li>
-          <% else %>
-            <li class="relative group">
-              <.link href={~p"/users/log_in"} tabindex="1" class="px-4 py-2 block hover:bg-gray-700 dark:hover:bg-gray-400 rounded-md transition-all">{gettext("Log in")}</.link>
-              <ul class="hidden group-hover:block group-hover:delay-150 md:absolute md:left-0 md:bg-gray-800 md:dark:bg-gray-300 md:mt-1 md:w-44 md:rounded-md md:shadow-lg md:border md:border-gray-700 md:dark:border-gray-400
-                md:p-0 p-2 bg-gray-900 dark:bg-gray-200 rounded-md md:shadow-none">
-                <li><.link href={~p"/users/register"} tabindex="1" class="block px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-400">{gettext("Register")}</.link></li>
-              </ul>
-            </li>
-          <% end %>
-
-        </ol>
+            <%= if @user_id do %>
+              <.dropdown
+                id="inDropdown"
+                label={@user_id.email}
+                items={[
+                  %{label: "Settings", href: ~p"/users/settings"},
+                  %{
+                    label: "Log out",
+                    href: ~p"/users/log_out",
+                    method: "DELETE",
+                    phx_hook: "LogoutButton"
+                  }
+                ]}
+              />
+            <% else %>
+              <.dropdown
+                id="outDropdown"
+                label="Log in"
+                items={[
+                  %{label: "Log in", href: ~p"/users/log_in"},
+                  %{label: "Register", href: ~p"/users/register"}
+                ]}
+              />
+            <% end %>
+          </ol>
+        </div>
       </div>
-    </div>
-  </nav>
-  """
+    </nav>
+    """
   end
 
   defp toggle_menu do
     %JS{}
     |> JS.toggle_class("hidden", to: "#menu")
     |> JS.toggle_attribute({"aria-expanded", "true", "false"}, to: "#menu-button")
+  end
+
+  def handle_event("delete", %{"href" => href}, socket) do
+    # Assuming you're logging out the user here
+    if href == "/users/log_out" do
+      # Log the user out
+      {:noreply, push_navigate(socket, to: "/")}
+    else
+      {:noreply, socket}
+    end
   end
 end
