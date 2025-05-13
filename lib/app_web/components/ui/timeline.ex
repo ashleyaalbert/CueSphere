827a974334ca3344cfaa9da030cd7f245ed292dc
ashley <aaa016@bucknell.edu>
2025-05-12 23:14:58 -0400

    Fixed timeline colors

--- lib/app_web/components/ui/timeline.ex
+++ lib/app_web/components/ui/timeline.ex
@@ -14,13 +14,13 @@ defmodule AppWeb.Components.UI.Timeline do
 
   def timeline(assigns) do
     ~H"""
-      <ol class="relative border-s border-gray-200 dark:border-gray-700">
+      <ol class="relative border-s border-gray-900 dark:border-gray-100">
       <%= for entry <- @entries do %>
         <li class="mb-10 ms-4">
-          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"><%= entry.date %></time>
+          <div class="absolute w-3 h-3 bg-gray-900 rounded-full mt-1.5 -start-1.5 border border-gray-900 dark:border-gray-100 dark:bg-gray-100"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-900 dark:text-white"><%= entry.date %></time>
           <h3 class="text-lg font-semibold text-gray-900 dark:text-white"><%= entry.title %></h3>
-          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"><%= entry.description %></p>
+          <p class="mb-4 text-base font-normal text-gray-800 dark:text-gray-100"><%= entry.description %></p>
           <%= if entry[:link] && entry[:link_text] do %>
             <a href={entry.link} class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
               <%= entry.link_text %>
